854f15a6ea2cea9916e306b1a4ac859dfd1ab24f
Carvalho Estéban <esteban.carvalho@grenoble-inp.fr>
2020-10-23 19:25:14 +0200

    Update
    - Remove map test (for future)
    - Update Fr Matlab Guide

--- webmaprando/layers/sommets_2.js
+++
@@ -1,8 +0,0 @@
-var json_sommets_2 = {
-"type": "FeatureCollection",
-"name": "sommets_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
